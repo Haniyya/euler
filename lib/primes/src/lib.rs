da8abeaf5b65ba18787ffc9f81ad303b67f4ae3d
Paul Martensen <stu102532@mail.uni-kiel.de>
2015-12-28 18:18:49 +0100

    Added gitignore and copied aks-prime-test

--- lib/primes/src/lib.rs
+++ lib/primes/src/lib.rs
@@ -1,3 +1,39 @@
+use std::iter::repeat;
+
+fn aks_coefficients(k: usize) -> Vec<i64> {
+    let mut coefficients = repeat(0i64).take(k+1).collect::<Vec<_>>();
+    coefficients[0] = 1;
+    for i in  1..(k+1) {
+        coefficients[i] = -(1..i).fold(coefficients[0], |prev,j| {
+            let old = coefficients[j];
+            coefficients[j] = old - prev;
+            old
+        });
+    }
+    coefficients
+}
+
+pub fn is_aks_prime(p: usize) -> bool {
+    if p < 2 {
+        false
+    } else {
+        let c = aks_coefficients(p);
+        (1 .. (c.len() - 1) / 2 + 1).all(|i| (c[i] % (p as i64)) == 0)
+    }
+}
+
 #[test]
-fn it_works() {
+fn small_prime() {
+    assert!(true == is_aks_prime(11))
+}
+
+#[test]
+fn below_two() {
+    assert!(false == is_aks_prime(0));
+    assert!(false == is_aks_prime(1));
+}
+
+#[test]
+fn big_prime() {
+    assert!(true == is_aks_prime((11 * 23) + 1))
 }
