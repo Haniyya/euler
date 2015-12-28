1afe17f03928ac667156c657de4ac1f103df6d68
Paul Martensen <stu102532@mail.uni-kiel.de>
2015-12-28 18:59:51 +0100

    Extracted aks function for better readability. Also implemented new naive function for performance comparison.

---
+++ lib/primes/src/naive/mod.rs
@@ -0,0 +1,37 @@
+pub fn is_naive_prime(n: usize) -> bool {
+    match n {
+        0 ... 1 => return false,
+        2 ... 3 => return true,
+        _ => {
+            if n % 2 == 0  || n % 3 == 0 {
+                return false;
+            } else {
+                let mut i = 5;
+
+                while i * i <= n {
+                    if n % i == 0 || n % (i+2) == 0 {
+                        return false;
+                    }
+                    i += 6;
+                }
+            }
+        }
+    }
+    true
+}
+
+#[test]
+fn below_two(){
+    assert!(false == is_naive_prime(0));
+    assert!(false == is_naive_prime(1))
+}
+
+#[test]
+fn actual_prime(){
+    let big = 2 * 3 * 5 * 7 * 11 + 1;
+    assert!(true == is_naive_prime(17));
+    assert!(true == is_naive_prime(big))
+}
+
+
+
